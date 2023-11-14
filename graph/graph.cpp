4d33d613fa1b09ebeae3c9c999a35f25f778379b
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2023-11-13 19:06:51 -0700

    deleted dfs function

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -1,43 +1,21 @@
 #include "graph.hpp"
 
 // Add an edge to the graph. Remember, this is a non-directed graph, so U <-> V
-void Graph::addEdge(int u, int v, std::vector<std::vector<bool>>& adj_matrix, std::list<int>* &adj_list) {
-    adj_matrix[u][v] = true;
-    adj_matrix[v][u] = true;
+void Graph::addEdge(int u, int v, std::list<int>* &adj_list) {
     adj_list[u].push_back(v);
     adj_list[v].push_back(u);
 }
 
-// Deep First Search - Recursive Function
-void Graph::recursiveDFS(int currentNode, std::vector<bool>& visited, std::vector<std::vector<bool>>& adj_matrix) {
-    visited[currentNode] = true;
-    std::cout << currentNode << " ";
-
-    for (size_t i = 0; i < adj_matrix.size(); ++i) {
-        if (adj_matrix[currentNode][i] && !visited[i]) {
-            recursiveDFS(i, visited, adj_matrix);
-        }
-    }
-}
-
 // Constructor of graph
-void Graph::loadGraph(int n, int m, std::vector<std::vector<bool>>& adj_matrix, std::list<int>* &adj_list) {
+void Graph::loadGraph(int n, int m, std::list<int>* &adj_list) {
     int u, v;
-    adj_matrix.assign(n, std::vector<bool>(n, false));
     adj_list =  new std::list<int>[n];
 
     for (size_t i = 0; i < m; ++i) {
         std::cout << "Introduce the edge " << i + 1 << ": ";
         std::cin >> u >> v;
-        addEdge(u, v, adj_matrix, adj_list);
+        addEdge(u, v, adj_list);
     }
-}
-
-// Deep First Search - Base Function
-void Graph::DFS(int start, std::vector<std::vector<bool>>& adj_matrix) {
-    std::vector<bool> visited(adj_matrix.size(), false);
-    recursiveDFS(start, visited, adj_matrix);
-    std::cout << std::endl;
 }
 
 // Breadth First Search
--- graph/graph.hpp
+++ graph/graph.hpp
@@ -7,10 +7,8 @@
 
 class Graph {
     private:
-        void addEdge(int u, int v, std::vector<std::vector<bool>>& adj_matrix, std::list<int>* &adj_list);
-        void recursiveDFS(int currentNode, std::vector<bool>& visited, std::vector<std::vector<bool>>& adj_matrix);
+        void addEdge(int u, int v, std::list<int>* &adj_list);
     public:
-        void loadGraph(int n, int m, std::vector<std::vector<bool>>& adj_matrix, std::list<int>* &adj_list);
-        void DFS(int start, std::vector<std::vector<bool>>& adj_matrix);
+        void loadGraph(int n, int m, std::list<int>* &adj_list);
         void BFS(int start,  std::list<int>* &adj_list);
 };
